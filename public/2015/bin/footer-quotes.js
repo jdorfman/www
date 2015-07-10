3df88be7688aac6d5def893829ec0ba386d5390a
Justin Dorfman <justindorfman@gmail.com>
2015-07-10 13:46:52 -0700

    hall of shame

---
+++ public/2015/bin/footer-quotes.js
@@ -0,0 +1,21 @@
+function msg () {
+  var theDate = new Date()
+  var today = theDate.getDay()
+
+  switch (today) {
+    case 1: document.write("Looks like somebody's got a case of the Mondays")
+      break
+    case 2: document.write('Only 3 more days till Friday...')
+      break
+    case 3: document.write('Hump Day!')
+      break
+    case 4: document.write('Throw it back!')
+      break
+    case 5: document.write("It's Friday, Friday everybody's looking forward to the weekend.")
+      break
+    case 6: document.write('Remember Saturday morning cartoons? #GoodOlDays')
+      break
+    case 0: document.write('Look at the bright side, you get to go to work tomorrow.')
+      break
+  }
+}
