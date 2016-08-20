ec685d7fcd92f6c972e06835965acef44609b46c
Justin Dorfman <justindorfman@gmail.com>
2016-08-19 18:27:08 -0700

    Added cssmin to grunt

--- public/2016/gruntFile.js
+++ public/2016/gruntFile.js
@@ -13,14 +13,26 @@ grunt.initConfig({
   },
   watch: {
     html: {
-      files: ['*.source.html'],
-      tasks: ['htmlmin']
+      files: ['*.source.html', 'assets/css/styles.css'],
+      tasks: ['htmlmin', 'cssmin']
     }
+  },
+  cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      cwd: 'assets/css',
+      src: ['*.css', '!*.min.css'],
+      dest: 'assets/css',
+      ext: '.min.css'
+    }]
   }
+}
 });
 
 grunt.loadNpmTasks('grunt-contrib-htmlmin');
 grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 grunt.registerTask('default', ['htmlmin']);
 
