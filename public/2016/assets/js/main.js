5a866b1a987bf7b2254a7f73297808f2259d61d9
Justin Dorfman <justindorfman@gmail.com>
2017-02-23 13:25:51 -0800

    Fixed js errors

--- public/2016/assets/js/main.js
+++ public/2016/assets/js/main.js
@@ -26,28 +26,28 @@ jQuery(document).ready(function($) {
 
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
 
-    $("#rss-feeds").rss(
-
-
-        "http://feeds.feedburner.com/TechCrunch/startups",
-
-        {
-        limit: 3,
-
-        effect: 'slideFastSynced',
-
-        layoutTemplate: "<div class='item'>{entries}</div>",
-        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-
-        }
-    );
+    // $("#rss-feeds").rss(
+    //
+    //
+    //     "http://feeds.feedburner.com/TechCrunch/startups",
+    //
+    //     {
+    //     limit: 3,
+    //
+    //     effect: 'slideFastSynced',
+    //
+    //     layoutTemplate: "<div class='item'>{entries}</div>",
+    //     entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+    //
+    //     }
+    // );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "jdorfman");
-
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "jdorfman", selector: "#ghfeed" });
+    // GitHubCalendar("#github-graph", "jdorfman");
+    //
+    //
+    // /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    // GitHubActivity.feed({ username: "jdorfman", selector: "#ghfeed" });
 
 
 });
